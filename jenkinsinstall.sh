0cf5cd1679b391836608a6947a9a089e9493e5b8
vagrant <vagrant@vm2.(none)>
2017-04-17 02:06:37 -0400

    JenkinsInstall

---
+++ jenkinsinstall.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+#Install Jenkins For Centos
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins
